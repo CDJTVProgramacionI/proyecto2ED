14693ba0a77f21fec8ecf7edd6617b54929c3bd9
DIRM2705 <rm202355047@alm.buap.mx>
2025-03-27 09:07:43 -0600

    Añade recorridos

--- main_screen.py
+++ main_screen.py
@@ -243,6 +243,83 @@ def act_kruskal(event):
     texto_distancia.value = f"El peso total del MST con Kruskal es: {peso_total}"
     texto_distancia.update()
     canvas.update()
+    
+    def suma(a : any, b : any) -> int:
+        if a == None or b == None:
+            return None
+        else:
+            return a + b
+    
+def lt(a : any, b : any) -> bool:
+    if b is None:
+        return True
+    elif a is None:
+        return False
+    else:
+        return a < b 
+#Dijsktra  
+def dijkstra(grafo : list, reciclaje: list, vertice_inicial : int):
+    cant_vertices = len(grafo)
+    s = [vertice_inicial] #Crea un arreglo con el vértice inicial
+    d = [None] * cant_vertices #Crea un arreglo de tamaño n 
+    p = [None] * cant_vertices            
+    
+    d[s[0]] = 0       
+    w = s[0]
+    for i in range(cant_vertices - 1):
+        menor_dist = None
+        for v in range(cant_vertices):
+            #Si w está en s, revisa el siguiente vértice
+            if v in s:
+                continue
+            
+            dist = suma(d[w], grafo[w][v])
+            if menor_dist is None:
+                menor_dist = v
+            else:
+                menor_dist = v if lt(dist, d[menor_dist]) else menor_dist
+            if grafo[w][v] != None and lt(dist, d[v]):
+                p[v] = w
+                d[v] = dist
+        w = menor_dist
+        s.append(w)
+
+    aux=0
+    for nodo in reciclaje: 
+        if nodo!=vertice_inicial and lt(d[nodo],d[aux]): 
+            aux=nodo
+    indi=aux
+    while(p[indi] != None): 
+        aux=p[indi]
+        x1 = lista_vertices[aux].left + 25
+        y1 = lista_vertices[aux].top + 25
+        x2 = lista_vertices[indi].left + 25
+        y2 = lista_vertices[indi].top + 25
+        genera_arista([x1,y1],[x2,y2],ft.Colors.AMBER_200)
+        indi=aux
+
+def floyd(grafo : list):
+    cant_vertices = len(grafo)
+    a = [[None] * cant_vertices for _ in range(cant_vertices)] #Crea un arreglo de tamaño n*n
+
+    #Inicializar matriz de distancias con la de costos
+    for i in range(cant_vertices):
+        for j in range(cant_vertices):
+            a[i][j] = grafo[i][j]
+            
+
+    for i in range(cant_vertices):
+        a[i][i] = 0
+
+    for k in range(cant_vertices):
+        for i in range(cant_vertices):
+            for j in range(cant_vertices):
+                dist = suma(a[i][k], a[k][j])
+                if lt(dist, a[i][j]):  
+                    a[i][j] = dist
+
+
+    return a #Retorna la matriz de distancias
         
 def buscar_componente(v2, componentes):
     for c2 in componentes:
